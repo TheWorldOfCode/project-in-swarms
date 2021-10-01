0cb02da2a68f40eeb6f068d8f75626dba330d1d2
TheWorldOfCode <dannj75@gmail.com>
2021-10-01 11:45:46 +0200

    Added experiments scripts

--- swarm/__main__.py
+++ swarm/__main__.py
@@ -1,10 +1,12 @@
 """ Main function """
 import logging
 import argparse
+import os
+import sys
 from random import seed
 
 from .world import World, WorldGenerator
-from .agent import random_agent_generator
+from .agents import random_agent_generator
 from . import VideoRecorder, DummyRecorder
 from . import Swarm
 from . import Simulator
@@ -13,31 +15,47 @@ from . import Simulator
 def args():
     parser = argparse.ArgumentParser(description="Solving the traveling Salesman problem using swarms.\n Autogenerate graph worlds with random weights and number of nodes")
 
-    parser.add_argument("-n", "--nodes",
-                        help="The minmum and maximum number of nodes in the world",
-                        nargs=2, metavar=("min", "max"), type=int, required=True)
+    subparsers = parser.add_subparsers(help="Commands")
+    subparse_run = subparsers.add_parser("run", help="Run a experiment")
 
-    parser.add_argument("-e", "--edge-multiplier",
-                        help="The number of edges is determine by the number of edges times the multiplier",
-                        nargs=1, metavar="multiplier", default=1, type=float)
+    subparse_run.add_argument("-n", "--nodes",
+                              help="The minmum and maximum number of nodes in the world",
+                              nargs=2, metavar=("min", "max"), type=int,
+                              required=True)
+    subparse_run.add_argument("-e", "--edge-multiplier",
+                              help="The number of edges is determine by the number of edges times the multiplier",
+                              nargs=1, metavar="multiplier", default=1,
+                              type=float)
+    subparse_run.add_argument("-c", "--edge-cost",
+                              help="The range of cost between nodes",
+                              nargs=2, metavar=("min", "max"), type=float,
+                              default=(1, 10))
+    subparse_run.add_argument("--seed",
+                              help="The seed for the autogeneration",
+                              nargs=1, metavar="seed", type=int,
+                              default=seed())
+    subparse_run.add_argument("--delay",
+                              help="The delay between each turn in the simulation (used together with display)",
+                              nargs=1, metavar="delay", type=float, default=-1)
+    subparse_run.add_argument("-r", "--record",
+                              help="Record a video of the simulation",
+                              nargs=1, metavar="filename", type=str)
 
-    parser.add_argument("-c", "--edge-cost",
-                        help="The range of cost between nodes",
-                        nargs=2, metavar=("min", "max"), type=float,
-                        default=(1, 10))
-    parser.add_argument("--seed",
-                        help="The seed for the autogeneration",
-                        nargs=1, metavar="seed", type=int, default=seed())
-    parser.add_argument("--delay",
-                        help="The delay between each turn in the simulation (used together with display)",
-                        nargs=1, metavar="delay", type=float, default=-1)
-    parser.add_argument("-r", "--record",
-                        help="Record a video of the simulation",
-                        nargs=1, metavar="filename", type=str)
+    subparse_run.add_argument("-s", "--swarm",
+                              help="The size of the swarm",
+                              nargs=1, metavar="size", type=int, required=True)
 
-    parser.add_argument("-s", "--swarm",
-                        help="The size of the swarm",
-                        nargs=1, metavar="size", type=int, required=True)
+    subparse_run.set_defaults(func=main)
+
+    subparse_scripts = subparsers.add_parser("script", help="Load an experiment from script")
+    group = subparse_scripts.add_mutually_exclusive_group(required=True)
+    group.add_argument("-l", "--load", nargs=1,
+                       help="Script to load", metavar="filename",
+                       type=str)
+    group.add_argument("--get-example",
+                       help="Get a example script", action="store_true")
+
+    subparse_scripts.set_defaults(func=scripts)
 
     return parser.parse_args()
 
@@ -78,7 +96,7 @@ def main(args):
                     display=args.delay != -1,
                     speed=args.delay, recording=recorder)
     results = sim.start(swarm)
-  
+
     half = 0
     turn = 0
     for i, t in enumerate(results.discovered):
@@ -103,6 +121,85 @@ def main(args):
     if args.record is not None:
         recorder.save(args.record)
 
+    return 0
+
+
+def scripts(args):
+    """TODO: Docstring for scripts.
+
+    :args: TODO
+    :returns: TODO
+
+    """
+
+    if args.load is not None:
+        import importlib.machinery as imp
+        logging.root.setLevel(logging.INFO)
+        logging.info(f"Runtime arguments f{args}")
+        script = args.load[0]
+        if os.path.exists(script):
+            logging.info(f"Loading {script}")
+            script_module_loader = imp.SourceFileLoader("script", script)
+            script_module = script_module_loader.load_module()
+
+            if not hasattr(script_module, "world_generation"):
+                logging.fatal("The script is missing the function world_generation, to generate the world")
+                return 1
+            elif not hasattr(script_module, "agent_generator_list"):
+                logging.fatal("The script is missing the function agent_generator_list, to generate list of geneators for the swarm")
+                return 1
+            elif not hasattr(script_module, "get_sim_args"):
+                logging.fatal("The script is missing the function get_sim_args, to get the arguments to the simulation")
+                return 1
+
+            world = script_module.world_generation()
+            gen_list = script_module.agent_generator_list()
+            sim_args = script_module.get_sim_args()
+            recorder = DummyRecorder()
+
+            print(sim_args)
+            if hasattr(script_module, "get_video_recorder"):
+                recorder = script_module.get_video_recorder()
+
+            swarm = Swarm(gen_list)
+            swarm.set_positions(0)
+
+            sim = Simulator(world, recording=recorder, **sim_args)
+            results = sim.start(swarm)
+
+            half = 0
+            turn = 0
+            for i, t in enumerate(results.discovered):
+                half += t
+                if half >= results.nodes/2:
+                    turn = i
+                    break
+
+            print()
+            print()
+            print("Simulations result")
+            print(f"\tNumber of turns: {results.turns}")
+            print(f"\tNumber of nodes: {results.nodes}")
+            print(f"\tAbove 50% at turn: {turn}")
+            print()
+            results = swarm.summary()
+            print("Swarm summary")
+            print(f"\tLowest traveling distance {results.lowest}")
+            print(f"\tMean traveling distance {results.mean} (±{results.std})")
+            print(f"\tLowest traveling distance {results.highest}")
+
+        else:
+            logging.fatal(f"Could not find {script}")
+            return 1
+    elif args.get_example:
+
+        from .example import get_example
+
+        sys.stdout.write(get_example())
+
+
+
 
 if __name__ == "__main__":
-    main(args())
+    arg = args()
+    arg.func(arg)
